02f137c5d4a1ffeb0e87139cc8b7bb916d52c187
saveasme1 <atoz0802@naver.com>
2026-07-24 02:01:38 +0900

    fix: tryon8 product load bust

--- tryon-overlay.js
+++ tryon-overlay.js
@@ -3,7 +3,7 @@
 
   const TRYON_ORIGIN = "https://saveasme1.github.io";
   const TRYON_PATH = "/heritage-tryon/studio.html";
-  const TRYON_BUST = "20260724-tryon7";
+  const TRYON_BUST = "20260724-tryon8";
   let overlay = null;
   let frame = null;
 
