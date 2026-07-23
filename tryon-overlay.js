500ec3e69fda73c472aa1abe40ff7d35bea74a32
saveasme1 <atoz0802@naver.com>
2026-07-24 02:35:52 +0900

    chore: tryon14 camera+compose

--- tryon-overlay.js
+++ tryon-overlay.js
@@ -3,7 +3,7 @@
 
   const TRYON_ORIGIN = "https://saveasme1.github.io";
   const TRYON_PATH = "/heritage-tryon/studio.html";
-  const TRYON_BUST = "20260724-tryon13";
+  const TRYON_BUST = "20260724-tryon14";
   let overlay = null;
   let frame = null;
 
