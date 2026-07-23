1ac01e62a332afc895ff7086cffdf656b5693597
saveasme1 <atoz0802@naver.com>
2026-07-24 02:23:09 +0900

    chore: tryon12 rembg+occlusion

--- tryon-overlay.js
+++ tryon-overlay.js
@@ -3,7 +3,7 @@
 
   const TRYON_ORIGIN = "https://saveasme1.github.io";
   const TRYON_PATH = "/heritage-tryon/studio.html";
-  const TRYON_BUST = "20260724-tryon10";
+  const TRYON_BUST = "20260724-tryon12";
   let overlay = null;
   let frame = null;
 
