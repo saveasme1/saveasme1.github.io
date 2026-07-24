861cccb3693d856372f9f3f99d0a2623d6489742
saveasme1 <atoz0802@naver.com>
2026-07-24 12:27:14 +0900

    chore: tryon19 bust after full asset restore

--- tryon-overlay.js
+++ tryon-overlay.js
@@ -3,7 +3,7 @@
 
   const TRYON_ORIGIN = "https://saveasme1.github.io";
   const TRYON_PATH = "/heritage-tryon/studio.html";
-  const TRYON_BUST = "20260724-tryon18";
+  const TRYON_BUST = "20260724-tryon19";
   let overlay = null;
   let frame = null;
   let overlayOpen = false;
