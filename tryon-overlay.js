634901fbd5123a35118bd21c2244fc71cde2f452
saveasme1 <atoz0802@naver.com>
2026-07-24 12:46:35 +0900

    fix: product load never blocks on gallery JSON (tryon23) (tryon-overlay.js)

--- tryon-overlay.js
+++ tryon-overlay.js
@@ -3,7 +3,7 @@
 
   const TRYON_ORIGIN = "https://saveasme1.github.io";
   const TRYON_PATH = "/heritage-tryon/studio.html";
-  const TRYON_BUST = "20260724-tryon22";
+  const TRYON_BUST = "20260724-tryon23";
   let overlay = null;
   let frame = null;
   let overlayOpen = false;
