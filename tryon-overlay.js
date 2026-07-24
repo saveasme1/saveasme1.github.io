e83525b31d649854901ceaaff614519aa0cb01e5
saveasme1 <atoz0802@naver.com>
2026-07-24 12:31:03 +0900

    feat: volumetric torus bracelet (tryon20) (tryon-overlay.js)

--- tryon-overlay.js
+++ tryon-overlay.js
@@ -3,7 +3,7 @@
 
   const TRYON_ORIGIN = "https://saveasme1.github.io";
   const TRYON_PATH = "/heritage-tryon/studio.html";
-  const TRYON_BUST = "20260724-tryon19";
+  const TRYON_BUST = "20260724-tryon20";
   let overlay = null;
   let frame = null;
   let overlayOpen = false;
