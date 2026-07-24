9e472daacf879fa0401b63f99498660ebe369c15
saveasme1 <atoz0802@naver.com>
2026-07-25 01:57:56 +0900

    fix: photo-matched dashed hand + 3D orange band + bracelet wrap insurance (tryon39) (tryon-overlay.js)

--- tryon-overlay.js
+++ tryon-overlay.js
@@ -3,7 +3,7 @@
 
   const TRYON_ORIGIN = "https://saveasme1.github.io";
   const TRYON_PATH = "/heritage-tryon/studio.html";
-  const TRYON_BUST = "20260725-tryon38";
+  const TRYON_BUST = "20260725-tryon39";
   let overlay = null;
   let frame = null;
   let overlayOpen = false;
