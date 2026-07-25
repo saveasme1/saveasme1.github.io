1b8325932b2a35a37e8ddbb19bf04f365a56ce54
saveasme1 <atoz0802@naver.com>
2026-07-26 00:50:29 +0900

    feat: live GLB AR loop, occlusion, 4-mode anchors, high-res save (tryon42) (heritage-tryon/src/services/ar/index.js)

--- heritage-tryon/src/services/ar/index.js
+++ heritage-tryon/src/services/ar/index.js
@@ -1,12 +1,17 @@
 export { estimateWristAnchor3D } from "./WristAnchorEstimator.js";
 export { estimateFingerAnchor3D } from "./FingerAnchorEstimator.js";
+export { estimateNeckAnchor3D } from "./NeckAnchorEstimator.js";
+export { estimateEarAnchor3D, estimateEarPair } from "./EarAnchorEstimator.js";
 export { TrackingSmoother } from "./TrackingSmoother.js";
 export { GuideOverlay } from "./GuideOverlay.js";
 export { GuideStateEngine } from "./GuideStateEngine.js";
 export { composeBracelet25D, composeRing25D } from "./BraceletFitter25D.js";
 export { loadJewelryMeta, resolveModelUrl, tryLoadGltf } from "./JewelryAssetLoader.js";
 export { detectQualityTier, QualityManager } from "./QualityManager.js";
-export { JewelryARRenderer } from "./JewelryARRenderer.js";
+export { JewelryARRenderer, resolveArProductId } from "./JewelryARRenderer.js";
 export { fitBracelet } from "./BraceletFitter3D.js";
 export { fitRing } from "./RingFitter3D.js";
+export { fitNecklace } from "./NecklaceFitter3D.js";
+export { fitEarring } from "./EarringFitter3D.js";
+export { composeHighResTryOn } from "./HighResCompose.js";
 export * from "./coord.js";
