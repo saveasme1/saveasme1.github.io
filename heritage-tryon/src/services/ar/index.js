fa6ed400ce9572c02aa5944a127dcdc6f71d1bc9
saveasme1 <atoz0802@naver.com>
2026-07-26 01:03:01 +0900

    feat: asset states, no validation leak, chain/physics, perf export (tryon43) (heritage-tryon/src/services/ar/index.js)

--- heritage-tryon/src/services/ar/index.js
+++ heritage-tryon/src/services/ar/index.js
@@ -9,9 +9,20 @@ export { composeBracelet25D, composeRing25D } from "./BraceletFitter25D.js";
 export { loadJewelryMeta, resolveModelUrl, tryLoadGltf } from "./JewelryAssetLoader.js";
 export { detectQualityTier, QualityManager } from "./QualityManager.js";
 export { JewelryARRenderer, resolveArProductId } from "./JewelryARRenderer.js";
+export {
+  resolveJewelryAsset,
+  resolveAssetCandidate,
+  ASSET_STATES,
+  isValidationId,
+} from "./AssetResolver.js";
 export { fitBracelet } from "./BraceletFitter3D.js";
 export { fitRing } from "./RingFitter3D.js";
 export { fitNecklace } from "./NecklaceFitter3D.js";
+export { fitNecklaceWithChain } from "./NecklaceChain.js";
 export { fitEarring } from "./EarringFitter3D.js";
+export { EarringPhysics } from "./EarringPhysics.js";
 export { composeHighResTryOn } from "./HighResCompose.js";
+export { PerfHarness } from "./PerfHarness.js";
+export { HAIR_OCCLUSION_DECISION, probeHairSegmenterCost } from "./HairOcclusionDecision.js";
+export { MATERIAL_PRESETS, GEMSTONE_TRADEOFF_KO } from "./MaterialPresets.js";
 export * from "./coord.js";
