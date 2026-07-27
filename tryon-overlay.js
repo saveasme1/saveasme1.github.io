440380dd7c97ec224f9c05123290e085d5176619
saveasme1 <atoz0802@naver.com>
2026-07-27 21:41:50 +0900

    feat: StyleAR Phase1 still-redetect, occlusion, harmonize (tryon46) (tryon-overlay.js)

--- tryon-overlay.js
+++ tryon-overlay.js
@@ -3,7 +3,7 @@
 
   const TRYON_ORIGIN = "https://saveasme1.github.io";
   const TRYON_PATH = "/heritage-tryon/studio.html";
-  const TRYON_BUST = "20260726-tryon45";
+  const TRYON_BUST = "20260727-tryon46";
   let overlay = null;
   let frame = null;
   let overlayOpen = false;
