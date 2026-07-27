741262f918aa6bf8e8c93d56a24c3a222172aa5e
saveasme1 <atoz0802@naver.com>
2026-07-27 22:17:37 +0900

    fix: harden try-on compose always produce canvas (tryon49) (tryon-overlay.js)

--- tryon-overlay.js
+++ tryon-overlay.js
@@ -3,7 +3,7 @@
 
   const TRYON_ORIGIN = "https://saveasme1.github.io";
   const TRYON_PATH = "/heritage-tryon/studio.html";
-  const TRYON_BUST = "20260727-tryon48";
+  const TRYON_BUST = "20260727-tryon49";
   let overlay = null;
   let frame = null;
   let overlayOpen = false;
